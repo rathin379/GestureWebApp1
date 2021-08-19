0f6e6f3531be01e890bb851f3fe8e92df1e0a2fa
rathin379 <77762779+rathin379@users.noreply.github.com>
2021-08-18 17:14:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 prediction_1 = "";
-prediction_2 = "";
 
 Webcam.set({
     width:350,
@@ -22,4 +21,62 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 
 function modelLoaded(){
     console.log('Model Loaded!');
+}
+
+function check(){
+    img= document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error); 
+    }else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label=="Good"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label=="Peace"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if(results[0].label=="OK"){
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label=="High Five"){
+            document.getElementById("update_emoji").innerHTML = "&#9995;";
+        }
+        if(results[0].label=="Bad"){
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+
+        if(results[1].label=="Good"){
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if(results[1].label=="Peace"){
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+        if(results[1].label=="OK"){
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        if(results[1].label=="High Five"){
+            document.getElementById("update_emoji2").innerHTML = "&#9995;";
+        }
+        if(results[1].label=="Bad"){
+            document.getElementById("update_emoji2").innerHTML = "&#128078;";
+        }
+
+    }
+}
+
+function speak(){
+    
+    var synth = window.speechSynthesis;
+    speak_data1 = "This hand gesture means "+prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1);
+    synth.speak(utterThis);
 }
